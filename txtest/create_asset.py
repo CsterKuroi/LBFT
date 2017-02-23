47668f815c2b3ac54e77f88d161e799ff692cddf
Kuroi <412681778@qq.com>
2017-02-24 00:31:12 +0800

    txtest redirects to github.com/uni-ledger/unichain-testcase

--- txtest/create_asset.py
+++
@@ -1,6 +0,0 @@
-from bigchaindb.common.transaction import Transaction, Asset
-from bigchaindb.common.util import serialize
-
-print(serialize(Asset().to_dict()))
-
-print(serialize(Asset(data={"bicycle": {"manufacturer":  "bkfab" ,"serial_number":  "abcd1234"}}).to_dict()))
